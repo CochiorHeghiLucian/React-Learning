fcda04cb7c3db8aea9b9ed265d5ab0e57dc41de8
CochiorHeghiLucian <lucian.cochiorheghi@gmail.com>
2019-03-09 12:52:49 +0200

    Addind rander components by a conditional expresion Outputting Lists

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ const app = props => {
       { name: "Max", age: 28 },
       { name: "Manu", age: 29 },
       { name: "Lucian", age: 22 }
-    ]
+    ],
+    showPersons: false
   });
 
   console.log(personsState);
@@ -19,7 +20,8 @@ const app = props => {
         { name: newName, age: 28 },
         { name: "Manu", age: 29 },
         { name: "Lucian", age: 20 }
-      ]
+      ],
+      showPersons: personsState.showPersons
     });
   };
 
@@ -29,7 +31,8 @@ const app = props => {
         { name: "Max", age: 28 },
         { name: event.target.value, age: 29 },
         { name: "Lucian", age: 26 }
-      ]
+      ],
+      showPersons: personsState.showPersons
     });
   };
 
@@ -38,32 +41,48 @@ const app = props => {
     font: "inherit",
     border: "1px solid blue",
     padding: "8px",
-    curson: 'pointer'
+    curson: "pointer"
   };
+
+  const togglePersonsHandler = event => {
+    setPersonsState({
+      persons: personsState.persons,
+      showPersons: !personsState.showPersons
+    });
+  };
+
+  let persons = null;
+  if (personsState.showPersons) {
+    persons = (
+      <div>
+        <Person
+          name={personsState.persons[0].name}
+          age={personsState.persons[0].age}
+        />
+        <Person
+          name={personsState.persons[1].name}
+          age={personsState.persons[1].age}
+          click={switchNameHandler.bind(this, "Max!")}
+          changed={nameChangedHandler}
+        >
+          My hobbies: Racing
+        </Person>
+        <Person
+          name={personsState.persons[2].name}
+          age={personsState.persons[2].age}
+        />
+      </div>
+    );
+  }
 
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is really working!</p>
-      <button style={style} onClick={() => switchNameHandler("Heghi!!")}>
-        Switch Name
+      <button style={style} onClick={() => togglePersonsHandler()}>
+        Toggle Persons
       </button>
-      <Person
-        name={personsState.persons[0].name}
-        age={personsState.persons[0].age}
-      />
-      <Person
-        name={personsState.persons[1].name}
-        age={personsState.persons[1].age}
-        click={switchNameHandler.bind(this, "Max!")}
-        changed={nameChangedHandler}
-      >
-        My hobbies: Racing
-      </Person>
-      <Person
-        name={personsState.persons[2].name}
-        age={personsState.persons[2].age}
-      />
+      {persons}
     </div>
   );
   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
